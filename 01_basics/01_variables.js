36a7bf0a38ca9c55bd00633ee63847dec3f4ff3c
kritikaapandey28 <134614637+kritikaapandey28@users.noreply.github.com>
2026-03-01 11:32:27 +0000

    comit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1232123
+let accountEmail = "kritika@google.com"
+var accountPassword = "34543"
+accountCity = "Gurugram"
+
+//accountId = 2;  Not allowed 
+
+// console.log(accountId)
+
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
+
+
